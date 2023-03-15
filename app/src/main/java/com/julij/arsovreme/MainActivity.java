1cc77b4103e779d6572ee4079d638cf129367b73
pro1mantis <julij.dominik@pm.me>
2023-03-15 10:28:28 +0100

    Adding files

---
+++ app/src/main/java/com/julij/arsovreme/MainActivity.java
@@ -0,0 +1,35 @@
+package com.julij.arsovreme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        web = findViewById(R.id.webview);
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        WebSettings settings = web.getSettings();
+        settings.setDomStorageEnabled(true);
+        web.setWebViewClient(new Callback());
+        web.loadUrl("https://vreme.arso.gov.si/napoved/Ljubljana/graf");
+
+    }
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
+    }
+}
